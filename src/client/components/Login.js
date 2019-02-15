134a43de3738af08913ab34c7d4c5931046e3822
TanguyScholtes <soultaker720@hotmail.com>
2019-02-15 10:24:19 +0100

    got all files back (hopefully... ?)

---
+++ src/client/components/Login.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
+/* becodeorg/bookshelf
+ *
+ * /src/client/components/hello.js - Hello Component
+ *
+ * coded by leny@BeCode
+ * started at 18/01/2019
+ */
+
+import * as React from "react";
+
+export default class Login extends React.Component {
+    render() {
+        // eslint-disable-next-line react/jsx-no-literals
+        return <div>Hi i'm LOGIN component</div>;
+    }
+}
