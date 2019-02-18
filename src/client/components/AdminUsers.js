179fe90688f5cfdd7844a5ddd0274707774bdc80
JVDB18 <vanderbysej@gmail.com>
2019-02-18 13:08:30 +0100

    merge Emilie + add axios, fix méthode handlecheck, props Edit, ajout des states Uri, display onclick des forms, modif router

--- src/client/components/AdminUsers.js
+++ src/client/components/AdminUsers.js
@@ -1,16 +1,22 @@
+/* eslint-disable no-dupe-class-members */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/jsx-no-literals */
 import React from "react";
-import {NavLink} from "react-router-dom";
 import Axios from "axios";
+import UsersEdit from "./UsersEdit";
+import UsersAdd from "./UsersAdd";
 
 export default class AdminUsers extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             users: [],
+            uri: "http://localhost/api",
+            show: "",
+            isCreate: "",
         };
         this.handleclick = this.handleclick.bind(this);
+        this.handleCreate = this.handleCreate.bind(this);
         // eslint-disable-next-line no-shadow
     }
     handleclick() {
@@ -24,10 +30,21 @@ export default class AdminUsers extends React.Component {
             });
         }
     }
+    handleCreate() {
+        if (this.state.isCreate === false) {
+            this.setState({
+                isCreate: true,
+            });
+        } else {
+            this.setState({
+                isCreate: false,
+            });
+        }
+    }
     componentDidMount() {
-        const uri = "http://localhost:80/api/users";
+        const uri = this.state.uri;
 
-        Axios.get(uri, {crossdomain: true}).then(response => {
+        Axios.get(`${uri}/users`, {crossdomain: true}).then(response => {
             console.log(response.data);
             this.setState({users: response.data});
             console.log(this.state.users);
@@ -40,10 +57,13 @@ export default class AdminUsers extends React.Component {
         }
         return (
             <>
-                I'M ADMIN COMPONENT
-                <NavLink to="/admin/add">
-                    <button>Add User</button>
-                </NavLink>
+                Users's list
+                <input
+                    type="button"
+                    value="Add User"
+                    onClick={this.handleCreate}
+                />
+                <UsersAdd isCreate={this.state.isCreate} uri={this.state.uri} />
                 <ul>
                     {this.state.users.map(user => (
                         <li key={user._id}>
@@ -51,6 +71,19 @@ export default class AdminUsers extends React.Component {
                             <br />
                             E-mail: {user.email}
                             <br />
+                            <input
+                                type="button"
+                                value="Edit"
+                                onClick={this.handleclick}
+                            />
+                            <UsersEdit
+                                uri={this.state.uri}
+                                show={this.state.show}
+                                id={user._id}
+                                pseudo={user.pseudo}
+                                email={user.email}
+                                isCoach={user.isCoach}
+                            />
                         </li>
                     ))}
                 </ul>
